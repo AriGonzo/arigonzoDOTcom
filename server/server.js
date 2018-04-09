5571e40ce082e5206479079f80023cdd9368eb78
Ari Gonzalez <arigonzalez515@gmail.com>
2018-04-09 14:25:55 -0400

    added openshift changes

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ var mongoose = require('mongoose');
 
 // Create Instance of Express
 var app = express();
-var PORT = process.env.PORT || 4000; // Sets an initial port. We'll use this later in our listener
-
+// var PORT = process.env.PORT || 4000; // Sets an initial port. We'll use this later in our listener
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 4000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 // Run Morgan for Logging
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,27 +19,27 @@ app.use(express.static('./node_modules'));
 
 // -------------------------------------------------
 
-// MongoDB Configuration configuration (Change this URL to your own DB)
-if (!process.env.NODE_ENV) {
-    mongoose.connect('mongodb://localhost:27017/ariGonzo');
-} else {
-	mongoose.connect('mongodb://heroku_w7m695c4:rjq0qm60jc9ctrt2latg5sajpe@ds121696.mlab.com:21696/heroku_w7m695c4')
-}
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
+// // MongoDB Configuration configuration (Change this URL to your own DB)
+// if (!process.env.NODE_ENV) {
+//     mongoose.connect('mongodb://localhost:27017/ariGonzo');
+// } else {
+// 	mongoose.connect('mongodb://heroku_w7m695c4:rjq0qm60jc9ctrt2latg5sajpe@ds121696.mlab.com:21696/heroku_w7m695c4')
+// }
+// mongoose.Promise = global.Promise;
+// var db = mongoose.connection;
 
-db.on('error', function (err) {
-  console.log('Mongoose Error: ', err);
-});
+// db.on('error', function (err) {
+//   console.log('Mongoose Error: ', err);
+// });
 
-db.once('open', function () {
-  console.log('Mongoose connection successful.');
-});
+// db.once('open', function () {
+//   console.log('Mongoose connection successful.');
+// });
 
 // Routes
 // require('./routes/apiRoutes.js')(app); 
 require('./routes/htmlRoutes.js')(app);
 
-app.listen(PORT, function(){
-	console.log('Listening on', PORT);
+app.listen(server_port, server_ip_address, function(){
+	console.log('Listening on', server_port);
 });
